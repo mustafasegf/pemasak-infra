33f33fab04383b4bcba5da1d9509b9af8456e2c0
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-09-20 18:45:17 +0700

    feat: added docker run

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
+use bollard::{container::StartContainerOptions, image::ListImagesOptions, Docker};
 use nixpacks::{
     create_docker_image,
     nixpacks::{builder::docker::DockerBuilderOptions, plan::generator::GeneratePlanOptions},
@@ -13,5 +16,31 @@ async fn main() -> Result<()> {
         ..Default::default()
     };
     let envs = vec![];
-    create_docker_image("./src/go-example", envs, &plan_options, &build_options).await
+    create_docker_image("./src/go-example", envs, &plan_options, &build_options).await?;
+
+    let docker = Docker::connect_with_local_defaults()?;
+
+    let images = &docker
+        .list_images(Some(ListImagesOptions::<String> {
+            all: false,
+            filters: HashMap::from([(
+                "reference".to_string(),
+                vec!["go-example:latest".to_string()],
+            )]),
+            ..Default::default()
+        }))
+        .await
+        .unwrap();
+
+    for image in images {
+        println!("-> {:#?}", image);
+    }
+
+    let _image = images.first().ok_or(anyhow::anyhow!("No image found"))?;
+
+    docker
+        .start_container("go-example", None::<StartContainerOptions<String>>)
+        .await?;
+
+    Ok(())
 }
