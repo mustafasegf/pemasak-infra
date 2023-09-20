ca3b36a842f57ace89d5c4e76cb88e937f7d58c5
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-09-20 16:34:01 +0700

    chore: added nix flakes build and devshell

--- flake.nix
+++ flake.nix
@@ -5,29 +5,55 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, crane, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        # this is how we can tell crane to use our toolchain!
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        # cf. https://crane.dev/API.html#libcleancargosource
+        src = craneLib.cleanCargoSource ./.;
+        # as before
+        nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
+        buildInputs = with pkgs; [ openssl ];
+        # because we'll use it for both `cargoArtifacts` and `bin`
+        commonArgs = {
+          inherit src buildInputs nativeBuildInputs;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        # remember, `set1 // set2` does a shallow merge:
+        bin = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
       in
       with pkgs;
       {
+        packages =
+          {
+            # that way we can build `bin` specifically,
+            # but it's also the default.
+            inherit bin;
+            default = bin;
+          };
         devShells.default = mkShell {
-
-          nativeBuildInputs = [
-            cmake
-            pkg-config
-          ];
-
-          buildInputs = [
-            openssl.dev
-            rust-bin.stable.latest.complete
-          ];
+          # instead of passing `buildInputs` / `nativeBuildInputs`,
+          # we refer to an existing derivation here
+          inputsFrom = [ bin ];
         };
       }
     );
