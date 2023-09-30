26ed49a21f16e8bb73a5d9eba60d953e8a3b9678
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-09-30 18:52:33 +0700

    feat: added owner and domains repository

--- schema.sql
+++ schema.sql
@@ -22,3 +22,51 @@ CREATE TABLE api_token (
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE owner (
+  id          uuid          NOT NULL,
+  name        TEXT          NOT NULL,
+  created_at  timestamptz   NOT NULL default now(),
+  updated_at  timestamptz   NOT NULL default now(),
+  deleted_at  timestamptz,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users_owner (
+  user_id     uuid          NOT NULL,
+  owner_id    uuid          NOT NULL,
+  created_at  timestamptz   NOT NULL default now(),
+  updated_at  timestamptz   NOT NULL default now(),
+  deleted_at  timestamptz,
+
+  PRIMARY KEY (user_id, owner_id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (owner_id) REFERENCES owner(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE repositories (
+  id          uuid          NOT NULL,
+  owner_id    uuid          NOT NULL,
+  name        TEXT          NOT NULL,
+  created_at  timestamptz   NOT NULL default now(),
+  updated_at  timestamptz   NOT NULL default now(),
+  deleted_at  timestamptz,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (owner_id) REFERENCES owner(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE domains (
+  id          uuid          NOT NULL,
+  repo_id     uuid          NOT NULL,
+  name        TEXT          NOT NULL,
+  created_at  timestamptz   NOT NULL default now(),
+  updated_at  timestamptz   NOT NULL default now(),
+  deleted_at  timestamptz,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (repo_id) REFERENCES repositories(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
