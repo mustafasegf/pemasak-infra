199252636d7b9fdddf23b6066c4ca6dd1e16cfb1
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-10-04 18:57:06 +0700

    style: format sql schema

--- schema.sql
+++ schema.sql
@@ -1,30 +1,31 @@
 CREATE TABLE users (
-  id          uuid          NOT NULL,
-  username    varchar(255)  NOT NULL,
-  password    varchar(255)  NOT NULL,
+  id          UUID          NOT NULL,
+  username    VARCHAR(255)  NOT NULL,
+  password    TEXT          NOT NULL,
   name        TEXT          NOT NULL,
-  created_at  timestamptz   NOT NULL default now(),
-  updated_at  timestamptz   NOT NULL default now(),
-  deleted_at  timestamptz,
+  created_at  TIMESTAMPTZ   NOT NULL default now(),
+  updated_at  TIMESTAMPTZ   NOT NULL default now(),
+  deleted_at  TIMESTAMPTZ,
 
   PRIMARY KEY (id),
   CONSTRAINT unique_username UNIQUE (username)
 );
 
 CREATE TABLE api_token (
-  id          uuid          NOT NULL,
-  user_id     uuid          NOT NULL,
+  id          UUID          NOT NULL,
+  user_id     UUID          NOT NULL,
+  name        VARCHAR(255)  NOT NULL,
   token       TEXT          NOT NULL,
-  created_at  timestamptz   NOT NULL default now(),
-  updated_at  timestamptz   NOT NULL default now(),
-  deleted_at  timestamptz,
+  created_at  TIMESTAMPTZ   NOT NULL default now(),
+  updated_at  TIMESTAMPTZ   NOT NULL default now(),
+  deleted_at  TIMESTAMPTZ,
 
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE user_permissions (
-  user_id    uuid NOT NULL,
+  user_id    UUID NOT NULL,
   token      VARCHAR(256) NOT NULL
 );
 
@@ -34,48 +35,48 @@ CREATE TABLE sessions (
   session TEXT NOT NULL
 );
 
-CREATE TABLE owner (
-  id          uuid          NOT NULL,
+CREATE TABLE owners (
+  id          UUID          NOT NULL,
   name        TEXT          NOT NULL,
-  created_at  timestamptz   NOT NULL default now(),
-  updated_at  timestamptz   NOT NULL default now(),
-  deleted_at  timestamptz,
+  created_at  TIMESTAMPTZ   NOT NULL default now(),
+  updated_at  TIMESTAMPTZ   NOT NULL default now(),
+  deleted_at  TIMESTAMPTZ,
 
   PRIMARY KEY (id)
 );
 
-CREATE TABLE users_owner (
-  user_id     uuid          NOT NULL,
-  owner_id    uuid          NOT NULL,
-  created_at  timestamptz   NOT NULL default now(),
-  updated_at  timestamptz   NOT NULL default now(),
-  deleted_at  timestamptz,
+CREATE TABLE users_owners (
+  user_id     UUID          NOT NULL,
+  owner_id    UUID          NOT NULL,
+  created_at  TIMESTAMPTZ   NOT NULL default now(),
+  updated_at  TIMESTAMPTZ   NOT NULL default now(),
+  deleted_at  TIMESTAMPTZ,
 
   PRIMARY KEY (user_id, owner_id),
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (owner_id) REFERENCES owner(id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 CREATE TABLE repositories (
-  id          uuid          NOT NULL,
-  owner_id    uuid          NOT NULL,
+  id          UUID          NOT NULL,
+  owner_id    UUID          NOT NULL,
   name        TEXT          NOT NULL,
-  created_at  timestamptz   NOT NULL default now(),
-  updated_at  timestamptz   NOT NULL default now(),
-  deleted_at  timestamptz,
+  created_at  TIMESTAMPTZ   NOT NULL default now(),
+  updated_at  TIMESTAMPTZ   NOT NULL default now(),
+  deleted_at  TIMESTAMPTZ,
 
   PRIMARY KEY (id),
-  FOREIGN KEY (owner_id) REFERENCES owner(id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE domains (
-  id          uuid          NOT NULL,
-  repo_id     uuid          NOT NULL,
+  id          UUID          NOT NULL,
+  repo_id     UUID          NOT NULL,
   name        TEXT          NOT NULL,
-  created_at  timestamptz   NOT NULL default now(),
-  updated_at  timestamptz   NOT NULL default now(),
-  deleted_at  timestamptz,
+  created_at  TIMESTAMPTZ   NOT NULL default now(),
+  updated_at  TIMESTAMPTZ   NOT NULL default now(),
+  deleted_at  TIMESTAMPTZ,
 
   PRIMARY KEY (id),
   FOREIGN KEY (repo_id) REFERENCES repositories(id) ON DELETE CASCADE ON UPDATE CASCADE
