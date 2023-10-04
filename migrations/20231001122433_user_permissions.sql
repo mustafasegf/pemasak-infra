e97a60cdef60174fa58eadc2f9e971c6690059ac
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-10-04 18:56:17 +0700

    chore: re init migrations. the old one is too long

--- migrations/20231001122433_user_permissions.sql
+++
@@ -1,2 +0,0 @@
--- Create "user_permissions" table
-CREATE TABLE "user_permissions" ("user_id" uuid NOT NULL, "token" character varying(256) NOT NULL);
--- migrations/20231001174806_sessions.sql
+++
@@ -1,2 +0,0 @@
--- Create "sessions" table
-CREATE TABLE "sessions" ("id" character varying(128) NOT NULL, "expires" integer NULL, "session" text NOT NULL, PRIMARY KEY ("id"));
