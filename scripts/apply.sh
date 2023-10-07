ae3ed700d26a1b09334b843b431ddf6ed9262e8a
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-10-07 20:40:57 +0700

    chore: added script for generating migration and applying migrations

---
+++ scripts/apply.sh
@@ -0,0 +1,16 @@
+#!//usr/bin/env bash
+json=$(cat configuration.yml| yj)
+# Extract values using jq
+db_user=$(echo "$json" | jq -r '.database.user')
+db_password=$(echo "$json" | jq -r '.database.password')
+db_port=$(echo "$json" | jq -r '.database.port')
+db_name=$(echo "$json" | jq -r '.database.name')
+# TODO: add support for ssl
+
+database_url="postgresql://$db_user:$db_password@localhost:$db_port/$db_name?search_path=public&sslmode=disable"
+
+echo "applying migrations to $database_url"
+
+atlas migrate apply \
+  --dir "file://migrations" \
+  --url "$database_url"
---
+++ scripts/migrate.sh
@@ -0,0 +1,7 @@
+#!//usr/bin/env bash
+echo "making migrations $@"
+
+atlas migrate diff $@ \
+  --dir "file://migrations" \
+  --to "file://schema.sql" \
+  --dev-url "docker://postgres/15/dev?search_path=public"
