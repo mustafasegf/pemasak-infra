4a830eba88200b62c66456ca6054149d2ef9e2b4
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-10-07 23:38:45 +0700

    refactor: change code from auth to it's own place

---
+++ src/components.rs
@@ -0,0 +1,31 @@
+use leptos::*;
+
+#[component]
+pub fn base(children: Children) -> impl IntoView{
+    view!{
+        <html data-theme="night">
+            <head>
+                <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+                // TODO: change tailwind to use node
+                <link href="https://cdn.jsdelivr.net/npm/daisyui@3.8.2/dist/full.css" rel="stylesheet" type="text/css" />
+                <script src="https://cdn.tailwindcss.com"></script>
+            </head>
+            <body>
+                // need this in body so body exist
+                <script> {"
+                    document.body.addEventListener('htmx:beforeSwap', function(evt) {{
+                      let status = evt.detail.xhr.status;
+                      if (status === 500 || status === 422 || status === 400) {{
+                        evt.detail.shouldSwap = true;
+                        evt.detail.isError = false;
+                      }}
+                    }});
+                "}</script>
+                //TODO: maybe make this optional
+                <div class="px-8 pt-8 pb-5 flex flex-col sm:px-12 md:px-24 lg:px-28 xl:mx-auto xl:max-w-6xl">
+                    {children()}
+                </div>
+            </body>
+        </html>
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,5 @@ pub mod telemetry;
 pub mod configuration;
 pub mod startup;
 pub mod auth;
+pub mod projects;
+pub mod components;
