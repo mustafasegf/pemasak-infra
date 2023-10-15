cd21afe1baf6cb07632b14c4de5633da5d4d5eba
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-10-15 22:39:46 +0700

    feat: change logging message

--- src/projects.rs
+++ src/projects.rs
@@ -9,12 +9,9 @@ use axum::{
 use axum_extra::routing::RouterExt;
 use garde::{Validate, Unvalidated};
 use serde_json::json;
-use axum_session::SessionPgPool;
-use axum_session_auth::AuthSession;
 use hyper::{Body, StatusCode};
 use leptos::{*, ssr::render_to_string};
-use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
+use serde::Deserialize;
 use ulid::Ulid;
 use uuid::Uuid;
 
@@ -24,7 +21,7 @@ use argon2::{
 };
 use rand::{Rng, SeedableRng};
 
-use crate::{startup::AppState, configuration::Settings, auth::{User, auth}, components::Base};
+use crate::{startup::AppState, configuration::Settings, auth::{auth, Auth}, components::Base};
 
 // Base64 url safe
 const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
@@ -73,10 +70,16 @@ pub async fn create_project(
     let owner_id = match sqlx::query!(
         r#"SELECT id FROM project_owners WHERE name = $1 AND deleted_at IS NULL"#,
         owner,
-    ).fetch_one(&pool).await {
-        Ok(data) => data.id,
+    ).fetch_optional(&pool).await {
+        Ok(Some(data)) => data.id,
+        Ok(None) => {
+            let html = render_to_string(move || { view! {
+                <h1> Owner does not exist </h1>
+            }}).into_owned();
+            return Response::builder().status(StatusCode::BAD_REQUEST).body(Body::from(html)).unwrap();
+        }
         Err(err) => {
-            tracing::error!("Failed to query database: {}", err);
+            tracing::error!(?err, "Can't get project_owners: Failed to query database");
             let html = render_to_string(move || { view! {
                 <h1> Failed to query database {err.to_string() } </h1>
             }}).into_owned();
@@ -89,8 +92,8 @@ pub async fn create_project(
         r#"SELECT id FROM projects WHERE name = $1 AND owner_id = $2"#,
         project,
         owner_id,
-    ).fetch_one(&pool).await {
-        Err(sqlx::Error::RowNotFound) => {},
+    ).fetch_optional(&pool).await {
+        Ok(None) => {},
         Ok(_) => {
             let html = render_to_string(move || { view! {
                 <h1> Project already exist</h1>
@@ -99,7 +102,7 @@ pub async fn create_project(
 
         },
         Err(err) => {
-            tracing::error!("Failed to query database: {}", err);
+            tracing::error!(?err, "Can't get projects: Failed to query database");
             let html = render_to_string(move || { view! {
                 <h1> Failed to query database {err.to_string() } </h1>
             }}).into_owned();
@@ -107,33 +110,44 @@ pub async fn create_project(
         }
     }
 
+    // TODO: create this into a tx and rollback if failed to create git repo
+    let mut tx = match pool.begin().await {
+        Ok(tx) => tx,
+        Err(err) => {
+            tracing::error!(?err, "Can't insert user: Failed to begin transaction");
+            let html = render_to_string(move || { view! {
+                <h1> Failed to begin transaction {err.to_string() } </h1>
+            }}).into_owned();
+
+            return Response::builder().status(StatusCode::BAD_REQUEST).header("Content-Type", "text/html").body(Body::from(html)).unwrap();
+        }
+    };
+
     // create project
     let project_id = match sqlx::query!(
         r#"INSERT INTO projects (id, name, owner_id) VALUES ($1, $2, $3) RETURNING id"#,
         Uuid::from(Ulid::new()),
         project,
         owner_id,
-    ).fetch_one(&pool).await {
+    ).fetch_one(&mut *tx).await {
         Ok(data) => data.id,
         Err(err) => {
-            tracing::error!("Failed to insert into database: {}", err);
+            tracing::error!(?err, "Can't insert projects: Failed to insert into database");
+            if let Err(err) = tx.rollback().await {
+                tracing::error!(?err, "Can't insert projects: Failed to rollback transaction");
+            }
+
             let html = render_to_string(move || { view! {
-                <h1> Failed to insert into database {err.to_string() } </h1>
+                <h1> Failed to insert into database</h1>
             }}).into_owned();
             return Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::from(html)).unwrap();
         }
     };
 
-    // if File::open(&path).is_ok() {
-    //     return Html(render_to_string(|| { view! {
-    //         <h1> project name already taken </h1>
-    //     }}).into_owned());
-    // };
-
     if let Err(err) =  git2::Repository::init_bare(path) {
-        tracing::error!("Failed to create repo: {}", err);
+        tracing::error!(?err, "Can't create project: Failed to create repo");
         let html = render_to_string(move || { view! {
-            <h1> Failed to create repo: {err.to_string() } </h1>
+            <h1> Failed to create project: {err.to_string() } </h1>
         }}).into_owned();
         return Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::from(html)).unwrap();
     }
@@ -150,7 +164,7 @@ pub async fn create_project(
     let hash = match hasher.hash_password(token.as_bytes(), &salt) {
         Ok(hash) => hash,
         Err(err) => {
-            tracing::error!("Failed to hash token: {}", err);
+            tracing::error!(?err, "Can't create project: Failed to hash token");
             let html = render_to_string(move || { view! {
                 <h1> Failed to generate token {err.to_string() } </h1>
             }}).into_owned();
@@ -158,18 +172,27 @@ pub async fn create_project(
         }
     };
 
-    if let Err(e) = sqlx::query!(
-        r#"INSERT INTO api_token (id, project_id, token) VALUES ($1, $2, $3)"#,
+    if let Err(err) = sqlx::query!(
+        "INSERT INTO api_token (id, project_id, token) VALUES ($1, $2, $3)",
         Uuid::from(Ulid::new()),
         project_id,
         hash.to_string(),
-    ).execute(&pool).await {
-        tracing::error!("Failed to insert into database: {}", e);
+    ).execute(&mut *tx).await {
+        tracing::error!(?err, "Can't insert api_token: Failed to insert into database");
         let html = render_to_string(move || { view! {
-            <h1> Failed to insert into database {e.to_string() } </h1>
+            <h1> Failed to insert into database {err.to_string() } </h1>
         }}).into_owned();
         return Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::from(html)).unwrap();
     };
+
+
+    if let Err(err) = tx.commit().await {
+        tracing::error!(?err, "Can't create project: Failed to commit transaction");
+        let html = render_to_string(move || { view! {
+            <h1> Failed to commit transaction {err.to_string() } </h1>
+        }}).into_owned();
+        return Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::from(html)).unwrap();
+    }
 
     let html = render_to_string(move || { view! {
         <h1> Project created successfully  </h1>
@@ -215,42 +238,25 @@ pub async fn create_project(
     Response::builder().status(StatusCode::OK).body(Body::from(html)).unwrap()
 }
 
-#[derive(sqlx::Type, Eq, PartialEq, Deserialize, Serialize, Debug)]
-pub struct Owner {
-    pub id: Uuid,
-    pub name: String,
-}
-
-#[derive(Eq, PartialEq, Deserialize, Serialize, Debug)]
-pub struct UserOwner {
-    pub id: Uuid,
-    pub name: String,
-    pub username: String,
-    pub owners: Vec<Owner>,
-}
-
 #[tracing::instrument(skip(auth, pool))]
 pub async fn create_project_ui(
-    auth: AuthSession<User, Uuid, SessionPgPool, PgPool>,
+    auth: Auth,
     State(AppState { pool, .. }): State<AppState>,
 ) -> Response<Body> {
     let user = auth.current_user.unwrap();
-    let user_owners: UserOwner = match sqlx::query_as!(
-        UserOwner,
-        r#"SELECT 
-            u.id, 
-            u.name, 
-            u.username,
-            COALESCE(NULLIF(ARRAY_AGG((o.id, o.name)), '{NULL}'), '{}') AS "owners!: Vec<Owner>" 
-          FROM users u, project_owners o
-          WHERE o.deleted_at is NULL
-            AND u.id = $1
-          GROUP BY u.id"#,
+
+    let owners = match sqlx::query!(
+        r#"select o.id, o.name
+           FROM project_owners o
+           JOIN users_owners uo on uo.owner_id = o.id
+           where uo.user_id = $1
+           AND o.deleted_at is NULL
+        "#,
         user.id
-    ).fetch_one(&pool).await {
+    ).fetch_all(&pool).await {
         Ok(data) => data,
         Err(err) => {
-            tracing::error!("Failed to query database: {}", err);
+            tracing::error!(?err, "Can't get owners: Failed to query database");
             let html = render_to_string(move || {
                 view! {
                     <h1> Failed to query database {err.to_string() } </h1>
@@ -269,14 +275,14 @@ pub async fn create_project_ui(
               class="flex flex-col mb-4 gap-2"
             >
                 <h1 class="text-2xl font-bold"> Create Project </h1>
-                <h3 class="text-lg"> {format!("login as {}", user.username)} </h3>
+                <h3 class="text-lg"> "login as " {user.username} </h3>
                 <div class="flex flex-row gap-2">
                     <div class="form-control">
                       <label class="label">
                         <span class="label-text">Owner</span>
                       </label>
                         <select name="owner" class="select select-bordered w-full max-w-xs">
-                            {user_owners.owners.into_iter().map(|owner|{ view!{ 
+                            {owners.into_iter().map(|owner|{ view!{ 
                                 <option>{owner.name}</option>
                             }}).collect::<Vec<_>>()}
                         </select>
@@ -299,26 +305,27 @@ pub async fn create_project_ui(
 
 #[tracing::instrument(skip(auth, pool))]
 pub async fn dashboard_ui(
-    auth: AuthSession<User, Uuid, SessionPgPool, PgPool>,
+    auth: Auth,
     State(AppState { pool, .. }): State<AppState>,
 ) -> Response<Body> {
     let user = auth.current_user.unwrap();
 
     let projects = match sqlx::query!(
-        r#"SELECT projects.name AS project , project_owners.name AS owner
-            FROM projects
-            JOIN project_owners ON projects.owner_id = project_owners.id
-            JOIN users_owners ON project_owners.id = users_owners.owner_id
-            JOIN users ON users_owners.user_id = users.id
-            WHERE users.id = $1"#,
+        r#"SELECT projects.name AS project, project_owners.name AS owner
+           FROM projects
+           JOIN project_owners ON projects.owner_id = project_owners.id
+           JOIN users_owners ON project_owners.id = users_owners.owner_id
+           JOIN users ON users_owners.user_id = users.id
+           WHERE users.id = $1
+        "#,
         user.id
     ).fetch_all(&pool).await {
         Ok(data) => data,
         Err(err) => {
-            tracing::error!("Failed to query database: {}", err);
+            tracing::error!(?err, "Can't get projects: Failed to query database");
             let html = render_to_string(move || {
                 view! {
-                    <h1> Failed to query database {err.to_string() } </h1>
+                    <h1> "Failed to query database "{err.to_string() } </h1>
                 }
             }).into_owned();
             return Response::builder().status(500).body(Body::from(html)).unwrap();
@@ -328,7 +335,7 @@ pub async fn dashboard_ui(
     let html = render_to_string(move || view! {
         <Base>
             <h1 class="text-2xl font-bold">Your Projects</h1>
-            <h3 class="text-lg"> {format!("login as {}", user.username)} </h3>
+            <h3 class="text-lg">"login as " {user.username}</h3>
             <div hx-boost="true" class="flex flex-col gap-4">
                 {projects.into_iter().map(|record|{ view!{ 
                 <div class="bg-neutral text-info py-4 px-8 w-full">
@@ -390,10 +397,19 @@ pub async fn delete_project_api(
     let owner_id = match sqlx::query!(
         r#"SELECT id FROM project_owners WHERE name = $1 AND deleted_at IS NULL"#,
         owner,
-    ).fetch_one(&pool).await {
-        Ok(data) => data.id,
+    ).fetch_optional(&pool).await {
+        Ok(Some(data)) => data.id,
+        Ok(None) => {
+            tracing::debug!("Can't delete project: Owner does not exist");
+            return Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from(
+                    json!({"message": "owner does not exist"}).to_string(),
+                ))
+                .unwrap();
+        }
         Err(err) => {
-            tracing::error!("Failed to query database: {}", err);
+            tracing::error!(?err, "Can't get project_owners: Failed to query database");
             return Response::builder()
                 .status(StatusCode::INTERNAL_SERVER_ERROR)
                 .body(Body::from(
@@ -442,12 +458,12 @@ pub async fn delete_project_api(
             .unwrap()
         }
         errs => {
-            return Response::builder()
+            Response::builder()
                 .status(StatusCode::INTERNAL_SERVER_ERROR)
                 .body(Body::from(
                     json!({"message": format!("failed to delete project: {:?}", errs)}).to_string(),
                 ))
-                .unwrap();
+                .unwrap()
         }
     }
 }
