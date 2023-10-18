9294286c905a5e2dd0aa0dbc20d8a29b95a857e7
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-10-19 02:26:39 +0700

    refactor(build-queue): refactor arc cloning

--- src/queue.rs
+++ src/queue.rs
@@ -1,4 +1,4 @@
-use std::{sync::{atomic::{AtomicUsize, Ordering}, Arc,}, collections::VecDeque, hash::Hash};
+use std::{sync::{atomic::{AtomicUsize, Ordering}, Arc,}, collections::{VecDeque, HashSet}, hash::Hash};
 
 use sqlx::PgPool;
 use tokio::sync::mpsc::{self, Receiver, Sender};
@@ -32,6 +32,7 @@ impl Eq for BuildItem {}
 pub struct BuildQueue {
     pub build_count: Arc<AtomicUsize>,
     pub waiting_queue: Arc<Mutex<VecDeque<BuildItem>>>,
+    pub waiting_set: Arc<Mutex<HashSet<BuildItem>>>,
     pub receive_channel: Receiver<(String, String, String, String)>,
     pub pg_pool: PgPool,
 }
@@ -43,142 +44,153 @@ impl BuildQueue {
         (Self {
             build_count: Arc::new(AtomicUsize::new(build_count)),
             waiting_queue: Arc::new(Mutex::new(VecDeque::new())),
+            waiting_set: Arc::new(Mutex::new(HashSet::new())),
             receive_channel: rx,
             pg_pool,
         }, tx)
     }
 }
 
-pub async fn process_task_poll(waiting_queue: Arc<Mutex<VecDeque<BuildItem>>>, build_count: Arc<AtomicUsize>, pool: PgPool) {
+pub async fn trigger_build(build_item: BuildItem, build_count: Arc<AtomicUsize>, pool: PgPool) {
+    let owner = &build_item.owner;
+    let repo = &build_item.repo;
+
+    let ip = match build_docker(&build_item.container_name, &build_item.container_src).await {
+        Ok(ip) => ip,
+        Err(err) => {
+            println!("error -> {:#?}", err);
+            build_count.fetch_add(1, Ordering::SeqCst);
+            return;
+        },
+    };
+    let project = match sqlx::query!(
+        r#"SELECT projects.id
+            FROM projects
+            JOIN project_owners ON projects.owner_id = project_owners.id
+            WHERE project_owners.name = $1
+            AND projects.name = $2"#,
+        owner,
+        repo
+    )
+    .fetch_optional(&pool)
+    .await
+    {
+        Ok(Some(project)) => project,
+        Err(err) => {
+            tracing::error!("failed to query database {}", err);
+            build_count.fetch_add(1, Ordering::SeqCst);
+            return;
+        },
+        Ok(None) => {
+            build_count.fetch_add(1, Ordering::SeqCst);
+            return;
+        }
+    };
+
+    let port: i32 = 80;
+
+    let subdomain = match sqlx::query!(
+        r#"SELECT domains.name
+           FROM domains
+           WHERE domains.project_id = $1"#,
+        project.id
+    )
+    .fetch_optional(&pool)
+    .await
+    {
+        Ok(Some(subdomain)) => subdomain.name,
+        Err(err) => {
+            tracing::error!("failed to query database {}", err);
+            build_count.fetch_add(1, Ordering::SeqCst);
+            return;
+        }
+        Ok(None) => {
+            // create domain
+            // TODO: clean up this mess
+            let subdomain = format!("{owner}-{repo}");
+            let id = Uuid::from(Ulid::new());
+            if let Err(err) = sqlx::query!(
+                r#"INSERT INTO domains (id, project_id, name, port, docker_ip)
+                   VALUES ($1, $2, $3, $4, $5)"#,
+                id,
+                project.id,
+                subdomain,
+                port,
+                ip,
+            )
+            .execute(&pool)
+            .await
+            {
+                tracing::error!("failed to query database {}", err);
+                build_count.fetch_add(1, Ordering::SeqCst);
+                return;
+            }
+            subdomain
+        }
+    };
+
+    build_count.fetch_add(1, Ordering::SeqCst);
+    println!("container run on {subdomain}");
+}
+
+pub async fn process_task_poll(waiting_queue: Arc<Mutex<VecDeque<BuildItem>>>, waiting_set: Arc<Mutex<HashSet<BuildItem>>>, build_count: Arc<AtomicUsize>, pool: PgPool) {
     loop {
         let mut waiting_queue = waiting_queue.lock().await;
-        let bc1 = Arc::clone(&build_count);
-        let bc2 = Arc::clone(&build_count);
+        let mut waiting_set = waiting_set.lock().await;
+        
+        let build_count = Arc::clone(&build_count);
 
-        if bc1.clone().load(Ordering::SeqCst) > 0 && waiting_queue.len() > 0 {
-            println!("BUILD TRIGGERED");
-            let build_item = waiting_queue.pop_front();
-            let pool = pool.clone();
+        if build_count.load(Ordering::SeqCst) > 0 && waiting_queue.len() > 0 {
+            let build_item = match waiting_queue.pop_front() {
+                Some(build_item) => build_item,
+                None => continue,
+            };
+            waiting_set.remove(&build_item);
 
-            bc1.fetch_sub(1, Ordering::SeqCst);
+            {
+                let build_count = Arc::clone(&build_count);
+                let pool = pool.clone();
 
-            tokio::spawn(async move {
-                println!("BUILD CALLED");
-
-                let build_item = match build_item {
-                    Some(build_item) => build_item,
-                    None => {
-                        bc2.fetch_add(1, Ordering::SeqCst);
-                        println!("NO BUILD ITEM");
-                        return;
-                    },
-                };
-
-                let owner = &build_item.owner;
-                let repo = &build_item.repo;
-
-                let ip = match build_docker(&build_item.container_name, &build_item.container_src).await {
-                    Ok(ip) => ip,
-                    Err(err) => {
-                        println!("error -> {:#?}", err);
-                        bc2.fetch_add(1, Ordering::SeqCst);
-                        return;
-                    },
-                };
-                let project = match sqlx::query!(
-                    r#"SELECT projects.id
-                        FROM projects
-                        JOIN project_owners ON projects.owner_id = project_owners.id
-                        WHERE project_owners.name = $1
-                        AND projects.name = $2"#,
-                    owner,
-                    repo
-                )
-                .fetch_optional(&pool)
-                .await
-                {
-                    Ok(Some(project)) => project,
-                    Err(err) => {
-                        tracing::error!("failed to query database {}", err);
-                        bc2.fetch_add(1, Ordering::SeqCst);
-                        return;
-                    },
-                    Ok(None) => {
-                        bc2.fetch_add(1, Ordering::SeqCst);
-                        return;
-                    }
-                };
-
-                let port: i32 = 80;
-
-                let subdomain = match sqlx::query!(
-                    r#"SELECT domains.name
-                       FROM domains
-                       WHERE domains.project_id = $1"#,
-                    project.id
-                )
-                .fetch_optional(&pool)
-                .await
-                {
-                    Ok(Some(subdomain)) => subdomain.name,
-                    Err(err) => {
-                        tracing::error!("failed to query database {}", err);
-                        bc2.fetch_add(1, Ordering::SeqCst);
-                        return;
-                    }
-                    Ok(None) => {
-                        // create domain
-                        // TODO: clean up this mess
-                        let subdomain = format!("{owner}-{repo}");
-                        let id = Uuid::from(Ulid::new());
-                        if let Err(err) = sqlx::query!(
-                            r#"INSERT INTO domains (id, project_id, name, port, docker_ip)
-                               VALUES ($1, $2, $3, $4, $5)"#,
-                            id,
-                            project.id,
-                            subdomain,
-                            port,
-                            ip,
-                        )
-                        .execute(&pool)
-                        .await
-                        {
-                            tracing::error!("failed to query database {}", err);
-                            bc2.fetch_add(1, Ordering::SeqCst);
-                            return;
-                        }
-                        subdomain
-                    }
-                };
-
-                println!("container run on {subdomain}");
-
-                bc2.fetch_add(1, Ordering::SeqCst);
-                println!("BUILD FINISHED");
-            });
+                build_count.fetch_sub(1, Ordering::SeqCst);
+                tokio::spawn(async move {    
+                    trigger_build(build_item, build_count, pool).await;
+                });
+            }
         }
     }
 }
 
-pub async fn process_task_enqueue(waiting_queue: Arc<Mutex<VecDeque<BuildItem>>>, mut receive_channel: Receiver<(String, String, String, String)>) {
+pub async fn process_task_enqueue(waiting_queue: Arc<Mutex<VecDeque<BuildItem>>>, waiting_set: Arc<Mutex<HashSet<BuildItem>>>, mut receive_channel: Receiver<(String, String, String, String)>) {
     while let Some(message) = receive_channel.recv().await {
         let (container_name, container_src, owner, repo) = message;
         let mut waiting_queue = waiting_queue.lock().await;
-        waiting_queue.push_back(BuildItem { container_name, container_src, owner, repo });
+        let waiting_set = waiting_set.lock().await;
+
+        let build_item = BuildItem { container_name, container_src, owner, repo };
+
+        if waiting_set.contains(&build_item) {
+            return
+        }
+
+        waiting_queue.push_back(build_item);
     }
 }
 
 pub async fn build_queue_handler(build_queue: BuildQueue) {
-    let queue_handle = build_queue.waiting_queue;
+    {
+        let waiting_queue = Arc::clone(&build_queue.waiting_queue);
+        let waiting_set: Arc<Mutex<HashSet<BuildItem>>> = Arc::clone(&build_queue.waiting_set);
 
-    let qh1 = Arc::clone(&queue_handle);
-    let qh2 = Arc::clone(&queue_handle);
-
-    tokio::spawn(async move {
-        process_task_poll(qh1, build_queue.build_count, build_queue.pg_pool).await;
-    });
-
-    tokio::spawn(async move {
-       process_task_enqueue(qh2, build_queue.receive_channel).await;
-    });
+        tokio::spawn(async move {
+            process_task_poll(waiting_queue, waiting_set, build_queue.build_count, build_queue.pg_pool).await;
+        });
+    }
+    {
+        let waiting_queue = Arc::clone(&build_queue.waiting_queue);
+        let waiting_set: Arc<Mutex<HashSet<BuildItem>>> = Arc::clone(&build_queue.waiting_set);
+        
+        tokio::spawn(async move {
+            process_task_enqueue(waiting_queue, waiting_set, build_queue.receive_channel).await;
+        });
+    }
 }
