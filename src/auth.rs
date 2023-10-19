d9c2e64eb6dce6ea17dcc5328fc97a693023de50
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-10-19 10:13:43 +0700

    refactor: build count decrement logic

--- src/auth.rs
+++ src/auth.rs
@@ -425,7 +425,6 @@ pub async fn register_user(
 
 #[tracing::instrument]
 pub async fn register_user_ui(State(AppState { build_channel, .. }): State<AppState>,) -> Html<String> {
-    let _ = build_channel.send(("A".to_string(), "B".to_string())).await;
     let html = render_to_string(|| {
         view! {
             <Base>
