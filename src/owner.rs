d9fa6f4cbd78ce5a5b2b8463955d8fde2f8a6d11
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-11-06 11:42:23 +0700

    feat(owners): implement member invite and removal API

--- src/owner.rs
+++ src/owner.rs
@@ -1,18 +1,23 @@
-use axum::{Router, extract::{State, Path}, response::Response, Form, routing::{post, put}};
+use axum::{
+    extract::{Path, State},
+    response::Response,
+    routing::post,
+    Form, Router,
+};
 use garde::{Unvalidated, Validate};
 use hyper::{Body, StatusCode};
-use leptos::{ssr::render_to_string, svg::view};
+use leptos::ssr::render_to_string;
+use leptos::*;
 use serde::Deserialize;
 use ulid::Ulid;
 use uuid::Uuid;
-use leptos::*;
 
-use crate::{configuration::Settings, startup::AppState, auth::Auth};
+use crate::{auth::Auth, configuration::Settings, startup::AppState};
 
 // TODO: separate schema for create and update when needed later on
 #[derive(Deserialize, Validate, Debug)]
 pub struct OwnerRequest {
-    #[garde(length(max=128))]
+    #[garde(length(max = 128))]
     pub name: String,
 }
 
@@ -20,33 +25,227 @@ pub struct OwnerRequest {
 pub async fn remove_project_member(
     auth: Auth,
     State(AppState { pool, .. }): State<AppState>,
-    Path((project_id, user_id)): Path<(Uuid, Uuid)>
+    Path((owner_id, user_id)): Path<(Uuid, Uuid)>,
 ) -> Response<Body> {
-    Response::builder().status(StatusCode::NO_CONTENT).body(Body::empty()).unwrap()
+    let authed_user_id = auth.id;
+
+    // Check if requesting user is already in owner group
+    match sqlx::query!(
+        r#"SELECT user_id, owner_id FROM users_owners
+        WHERE user_id = $1 AND owner_id = $2
+        "#,
+        authed_user_id,
+        owner_id
+    )
+    .fetch_optional(&pool)
+    .await
+    {
+        Ok(Some(_)) => (),
+        Ok(None) => {
+            tracing::error!(
+                "Can't find existing user_owner with user_id {} and owner_id {}",
+                user_id,
+                owner_id,
+            );
+
+            return Response::builder()
+                .status(StatusCode::UNAUTHORIZED)
+                .body(Body::empty())
+                .unwrap();
+        }
+        Err(err) => {
+            tracing::error!(
+                ?err,
+                "Can't get existing user_owner: Failed to query database"
+            );
+
+            return Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::empty())
+                .unwrap();
+        }
+    }
+
+    if let Err(err) = sqlx::query!(
+        r#"DELETE FROM users_owners
+        WHERE user_id = $1 AND owner_id = $2"#,
+        user_id,
+        owner_id
+    )
+    .execute(&pool)
+    .await
+    {
+        tracing::error!(
+            ?err,
+            "Can't delete users_owners: Failed to insert into database"
+        );
+
+        let html = render_to_string(move || {
+            view! {
+                <h1> Failed to remove owner group member </h1>
+            }
+        })
+        .into_owned();
+
+        return Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .header("Content-Type", "text/html")
+            .body(Body::from(html))
+            .unwrap();
+    };
+
+    Response::builder()
+        .status(StatusCode::NO_CONTENT)
+        .body(Body::empty())
+        .unwrap()
 }
 
 #[tracing::instrument(skip(auth, pool))]
 pub async fn invite_project_member(
     auth: Auth,
     State(AppState { pool, .. }): State<AppState>,
-    Path((project_id, user_id)): Path<(Uuid, Uuid)>
+    Path((owner_id, user_id)): Path<(Uuid, Uuid)>,
 ) -> Response<Body> {
-    Response::builder().status(StatusCode::NO_CONTENT).body(Body::empty()).unwrap()
+    let authed_user_id = auth.id;
+
+    // Check if requesting user is already in owner group
+    match sqlx::query!(
+        r#"SELECT user_id, owner_id FROM users_owners
+        WHERE user_id = $1 AND owner_id = $2
+        "#,
+        authed_user_id,
+        owner_id
+    )
+    .fetch_optional(&pool)
+    .await
+    {
+        Ok(Some(_)) => (),
+        Ok(None) => {
+            tracing::error!(
+                "Can't find existing user_owner with user_id {} and owner_id {}",
+                user_id,
+                owner_id,
+            );
+
+            return Response::builder()
+                .status(StatusCode::UNAUTHORIZED)
+                .body(Body::empty())
+                .unwrap();
+        }
+        Err(err) => {
+            tracing::error!(
+                ?err,
+                "Can't get existing user_owner: Failed to query database"
+            );
+
+            return Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::empty())
+                .unwrap();
+        }
+    }
+
+    let mut tx = match pool.begin().await {
+        Ok(tx) => tx,
+        Err(err) => {
+            tracing::error!(
+                ?err,
+                "Can't insert users_owners: Failed to begin transaction"
+            );
+            let html = render_to_string(move || {
+                view! {
+                    <h1> Failed to begin transaction {err.to_string()} </h1>
+                }
+            })
+            .into_owned();
+
+            return Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .header("Content-Type", "text/html")
+                .body(Body::from(html))
+                .unwrap();
+        }
+    };
+
+    if let Err(err) = sqlx::query!(
+        r#"INSERT INTO users_owners (user_id, owner_id)
+        VALUES ($1, $2)"#,
+        user_id,
+        owner_id,
+    )
+    .execute(&mut *tx)
+    .await
+    {
+        tracing::error!(
+            ?err,
+            "Can't insert users_owners: Failed to insert into database"
+        );
+
+        let error_html = match err {
+            sqlx::Error::Database(err) => {
+                if err.constraint() == Some("users_owners_pkey") {
+                    render_to_string(move || {
+                        view! {
+                            <h1> User is already in the owner group </h1>
+                        }
+                    })
+                    .into_owned()
+                } else {
+                    render_to_string(move || {
+                        view! {
+                            <h1> Failed to invite member to owner group </h1>
+                        }
+                    })
+                    .into_owned()
+                }
+            }
+            _ => render_to_string(move || {
+                view! {
+                    <h1> Failed to invite member to owner group </h1>
+                }
+            })
+            .into_owned(),
+        };
+
+        if let Err(err) = tx.rollback().await {
+            tracing::error!(
+                ?err,
+                "Can't insert users_owners: Failed to rollback transaction"
+            );
+        }
+
+        return Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .header("Content-Type", "text/html")
+            .body(Body::from(error_html))
+            .unwrap();
+    }
+
+    Response::builder()
+        .status(StatusCode::NO_CONTENT)
+        .body(Body::empty())
+        .unwrap()
 }
 
 #[tracing::instrument(skip(_auth, pool))]
 pub async fn create_project_owner(
     _auth: Auth,
     State(AppState { pool, .. }): State<AppState>,
-    Form(req):  Form<Unvalidated<OwnerRequest>>,
+    Form(req): Form<Unvalidated<OwnerRequest>>,
 ) -> Response<Body> {
     let data = match req.validate(&()) {
         Ok(valid) => valid.into_inner(),
         Err(err) => {
-            let html = render_to_string(move || { view! {
-                <p> {err.to_string() } </p>
-            }}).into_owned();
-            return Response::builder().status(StatusCode::BAD_REQUEST).body(Body::from(html)).unwrap();
+            let html = render_to_string(move || {
+                view! {
+                    <p> {err.to_string() } </p>
+                }
+            })
+            .into_owned();
+            return Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from(html))
+                .unwrap();
         }
     };
 
@@ -62,35 +261,57 @@ pub async fn create_project_owner(
     {
         Ok(None) => (),
         Ok(Some(_)) => {
-            tracing::error!("Project owner already exists with the following name: {}", data.name);
+            tracing::error!(
+                "Project owner already exists with the following name: {}",
+                data.name
+            );
 
-            let html = render_to_string(move || { view! {
-                <p> Project with name {data.name} already exists </p>
-            }}).into_owned();
+            let html = render_to_string(move || {
+                view! {
+                    <p> Project with name {data.name} already exists </p>
+                }
+            })
+            .into_owned();
 
-            return Response::builder().status(StatusCode::BAD_REQUEST).body(Body::from(html)).unwrap();
-        },
+            return Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from(html))
+                .unwrap();
+        }
         Err(err) => {
-            tracing::error!(?err, "Can't get existing project owner: Failed to query database");
+            tracing::error!(
+                ?err,
+                "Can't get existing project owner: Failed to query database"
+            );
 
             return Response::builder()
                 .status(StatusCode::BAD_REQUEST)
                 .body(Body::empty())
-                .unwrap()
+                .unwrap();
         }
     };
 
-    let project_id = Uuid::from(Ulid::new());
+    let owner_id = Uuid::from(Ulid::new());
 
     let mut tx = match pool.begin().await {
         Ok(tx) => tx,
         Err(err) => {
-            tracing::error!(?err, "Can't insert project owner: Failed to begin transaction");
-            let html = render_to_string(move || { view! {
-                <h1> Failed to begin transaction {err.to_string()} </h1>
-            }}).into_owned();
+            tracing::error!(
+                ?err,
+                "Can't insert project owner: Failed to begin transaction"
+            );
+            let html = render_to_string(move || {
+                view! {
+                    <h1> Failed to begin transaction {err.to_string()} </h1>
+                }
+            })
+            .into_owned();
 
-            return Response::builder().status(StatusCode::BAD_REQUEST).header("Content-Type", "text/html").body(Body::from(html)).unwrap();
+            return Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .header("Content-Type", "text/html")
+                .body(Body::from(html))
+                .unwrap();
         }
     };
 
@@ -98,24 +319,39 @@ pub async fn create_project_owner(
         r#"INSERT INTO project_owners (id, name)
         VALUES ($1, $2)
         "#,
-        project_id,
+        owner_id,
         data.name
     )
     .execute(&mut *tx)
-    .await {
-        tracing::error!(?err, "Can't insert project owner: Failed to insert into database");
+    .await
+    {
+        tracing::error!(
+            ?err,
+            "Can't insert project owner: Failed to insert into database"
+        );
         if let Err(err) = tx.rollback().await {
-            tracing::error!(?err, "Can't insert project owner: Failed to rollback transaction");
+            tracing::error!(
+                ?err,
+                "Can't insert project owner: Failed to rollback transaction"
+            );
         }
 
-        let html = render_to_string(move || { view! {
-            <h1> Failed to insert project owner into database </h1>
-        }}).into_owned();
+        let html = render_to_string(move || {
+            view! {
+                <h1> Failed to insert project owner into database </h1>
+            }
+        })
+        .into_owned();
 
-        return Response::builder().status(StatusCode::BAD_REQUEST).header("Content-Type", "text/html").body(Body::from(html)).unwrap();
+        return Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .header("Content-Type", "text/html")
+            .body(Body::from(html))
+            .unwrap();
     }
 
-    Response::builder().status(StatusCode::OK)
+    Response::builder()
+        .status(StatusCode::OK)
         .header("Content-Type", "text/html")
         .body(Body::empty())
         .unwrap()
@@ -125,15 +361,21 @@ pub async fn create_project_owner(
 pub async fn update_project_owner(
     auth: Auth,
     State(AppState { pool, .. }): State<AppState>,
-    Path(project_id): Path<String>,
-    Form(req): Form<Unvalidated<OwnerRequest>>
+    Path(owner_id): Path<String>,
+    Form(req): Form<Unvalidated<OwnerRequest>>,
 ) -> Response<Body> {
-    Response::builder().status(StatusCode::NO_CONTENT).body(Body::empty()).unwrap()
+    Response::builder()
+        .status(StatusCode::NO_CONTENT)
+        .body(Body::empty())
+        .unwrap()
 }
 
-pub fn router(_state: AppState, _config: &Settings) -> Router<AppState, Body> {    
+pub fn router(_state: AppState, _config: &Settings) -> Router<AppState, Body> {
     Router::new()
         .route("/owner", post(create_project_owner))
-        .route("/owner/:project_id", post(update_project_owner))
-        .route("/owner/:project_id/:user_id", post(invite_project_member).delete(remove_project_member))
+        .route("/owner/:owner_id", post(update_project_owner))
+        .route(
+            "/owner/:owner_id/:user_id",
+            post(invite_project_member).delete(remove_project_member),
+        )
 }
