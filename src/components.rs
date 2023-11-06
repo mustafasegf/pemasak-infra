fda0a5383ece4e861df9c567f88dfe0e8601242d
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-11-07 01:27:11 +0700

    feat(ui): implement site navigation components

--- src/components.rs
+++ src/components.rs
@@ -1,8 +1,8 @@
 use leptos::*;
 
 #[component]
-pub fn base(children: Children) -> impl IntoView{
-    view!{
+pub fn base(children: Children) -> impl IntoView {
+    view! {
         <html data-theme="night">
             <head>
                 <script src="https://unpkg.com/htmx.org@1.9.6"></script>
@@ -22,9 +22,29 @@ pub fn base(children: Children) -> impl IntoView{
                       }}
                     }});
                 "}</script>
-                //TODO: maybe make this optional
-                <div class="px-8 pt-8 pb-5 flex flex-col sm:px-12 md:px-24 lg:px-28 xl:mx-auto xl:max-w-6xl">
-                    {children()}
+
+                <div class="drawer">
+                    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+                    <div class="drawer-content">
+                        <div class="navbar bg-base-200 px-8 space-x-2">
+                            <label for="my-drawer" class="btn btn-neutral drawer-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                </svg>
+                            </label>
+                            <a href="/dashboard" class="btn btn-ghost normal-case text-xl">InfraCook</a>
+                        </div>
+                        <div class="px-8 pt-8 pb-5 flex flex-col sm:px-12 md:px-24 lg:px-28 xl:mx-auto xl:max-w-6xl">
+                            {children()}
+                        </div>
+                    </div>
+                    <div class="drawer-side" hx-boost="true">
+                        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+                        <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                            <li><a href="/dashboard">Dashboard</a></li>
+                            <li><a href="/owner">Owner Groups</a></li>
+                        </ul>
+                    </div>
                 </div>
             </body>
         </html>
