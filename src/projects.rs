3bfc8049a988535eef85e99eb3e05d189a984720
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-11-29 01:14:52 +0700

    feat(project-ui): Implement Project UI Build Log

--- src/projects.rs
+++ src/projects.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Cow, collections::HashMap, fs::File, net::SocketAddr, time::Duration};
+use std::{borrow::Cow, collections::HashMap, fs::File, net::SocketAddr, time::Duration, fmt};
 use tokio::io::AsyncWriteExt;
 
 use axum::{
@@ -61,6 +61,26 @@ pub struct CreateProjectRequest {
     pub owner: String,
     #[garde(alphanumeric)]
     pub project: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, sqlx::Type)]
+#[sqlx(type_name = "build_state", rename_all = "lowercase")] 
+pub enum BuildState {
+    PENDING,
+    BUILDING,
+    SUCCESSFUL,
+    FAILED
+}
+
+impl fmt::Display for BuildState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            BuildState::PENDING => write!(f, "Pending"),
+            BuildState::BUILDING => write!(f, "Building"),
+            BuildState::SUCCESSFUL => write!(f, "Successful"),
+            BuildState::FAILED => write!(f, "Failed"),
+        }
+    }
 }
 
 #[tracing::instrument(skip(pool, base, domain))]
@@ -501,8 +521,8 @@ pub async fn project_ui(
     let volume_path = format!("/{owner}/{project}/volume/delete");
 
     // check if project exist
-    let _project_rec = match sqlx::query!(
-        r#"SELECT projects.name AS project, project_owners.name AS owner
+    let project_record = match sqlx::query!(
+        r#"SELECT projects.id, projects.name AS project, project_owners.name AS owner
            FROM projects
            JOIN project_owners ON projects.owner_id = project_owners.id
            JOIN users_owners ON project_owners.id = users_owners.owner_id
@@ -545,24 +565,87 @@ pub async fn project_ui(
         }
     };
 
+    let builds = match sqlx::query!(
+        r#"SELECT id, project_id, status AS "status: BuildState", created_at, finished_at 
+        FROM builds WHERE project_id = $1
+        ORDER BY created_at DESC"#,
+        project_record.id
+    )
+    .fetch_all(&pool)
+    .await 
+    {
+        Ok(records) => records,
+        Err(err) => {
+            let html = render_to_string(move || {
+                view! {
+                    <h1> "Failed to query database " {err.to_string() } </h1>
+                }
+            })
+            .into_owned();
+            return Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from(html))
+                .unwrap();
+        }, 
+    };
+
     let html = render_to_string(move || {
         view! {
             <Base>
-              <h1> {owner}"/"{project} </h1>
-              <button
-                hx-post={delete_path}
-                hx-trigger="click"
-                class="btn btn-error mt-4 w-full max-w-xs"
-              >Delete Project</button>
+              <h1 class="text-2xl mb-8">{owner}"/"{project}</h1>
+              
+              <h2 class="text-xl">
+                Project Controls
+              </h2>
+              <div class="flex w-full space-x-4 items-center mb-8">
+                <button
+                  hx-post={delete_path}
+                  hx-trigger="click"
+                  class="btn btn-error mt-4 w-full max-w-xs"
+                >Delete Project</button>
 
-              <button
-                hx-post={volume_path}
-                hx-trigger="click"
-                class="btn btn-error mt-4 w-full max-w-xs"
-              >Delete Database</button>
+                <button
+                  hx-post={volume_path}
+                  hx-trigger="click"
+                  class="btn btn-error mt-4 w-full max-w-xs"
+                >Delete Database</button>
+              </div>
 
               <div id="result"></div>
 
+              <h2 class="text-xl">
+                Builds
+              </h2>
+              <div class="flex flex-col gap-4 w-full mt-4">
+                {builds.into_iter().enumerate().map(|(index, record)| { view!{
+                    <div class="bg-neutral text-info py-4 px-8 cursor-pointer w-full rounded-lg transition-all outline outline-transparent hover:outline-blue-500">
+                        {
+                            let id = record.id.to_string();
+                            let status = record.status.to_string();
+                            let created_at = record.created_at;
+                            view!{
+                                <a class="text-sm">
+                                    <h2 class="font-bold text-white">
+                                        <span>{id}</span>
+                                        {
+                                            let latest_build = match index == 0 {
+                                                true => " (LATEST BUILD)",
+                                                false => ""
+                                            };
+
+                                            view!{
+                                                <span class="text-info">{latest_build}</span>
+                                            }
+                                        }
+                                    </h2>
+                                    <p class="text-sm text-neutral-content">{"Status: "}{status}</p>
+                                    <p class="text-sm text-neutral-content">{"Started at: "}{created_at.to_rfc2822()}</p>
+                                </a>
+                            }
+                        }
+                    </div>
+                }}).collect::<Vec<_>>()}
+              </div>
             </Base>
         }
     })
