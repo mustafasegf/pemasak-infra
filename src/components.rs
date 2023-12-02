359e262a71d5eba181575ee7519752d7f5f6ccf7
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-12-02 16:22:00 +0700

    feat(ui): implement better web terminal ui

--- src/components.rs
+++ src/components.rs
@@ -31,8 +31,9 @@ pub fn base(children: Children) -> impl IntoView {
                     <div class="drawer-content">
                         <div class="w-full fixed">  
                             <div hx-boost="true" class="navbar bg-transparent px-8 py-6 space-x-2 mx-auto xl:max-w-6xl w-full">
-                                <img class="w-12 h-12" src="/assets/InfraCook.png"></img>
-                                <a href="/dashboard" class="btn btn-ghost normal-case text-xl">InfraCook</a>
+                                <a href="/dashboard" class="btn btn-ghost normal-case text-xl px-0">
+                                    <img class="w-12 h-12" src="/assets/InfraCook.png"></img>
+                                </a>
                             </div>
                         </div>
                         <div class="px-8 pt-32 pb-5 flex flex-col xl:mx-auto xl:max-w-6xl w-full min-h-screen">
