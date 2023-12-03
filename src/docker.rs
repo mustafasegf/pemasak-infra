2aaea46f60fcb9e6ec3c5c1f63be2eedd91a2960
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-12-03 19:02:22 +0700

    fix(build-queue): fix build queue timeout causing deadlock

--- src/docker.rs
+++ src/docker.rs
@@ -124,11 +124,14 @@ pub async fn build_docker(
         }
         false => {
             tracing::debug!(container_name, "Build using nixpacks");
+            let container_src = container_src.to_string();
             let Output {
                 status,
                 stderr,
                 stdout: _,
-            } = create_docker_image(container_src, envs, &plan_options, &build_options).await?;
+            } = tokio::task::spawn_blocking(move || {
+                create_docker_image(&container_src, envs, &plan_options, &build_options)
+            }).await?.unwrap();
 
             let build_log = String::from_utf8(stderr).unwrap();
 
