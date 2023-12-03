f089db5c71649c27ea811012a0cbcdca9f76195d
Adrian Ardizza <adrian.ardizza@gmail.com>
2023-12-04 01:12:37 +0700

    feat(build-log): implement build log page

--- src/projects/mod.rs
+++ src/projects/mod.rs
@@ -11,11 +11,13 @@ mod delete_project;
 mod delete_volume;
 mod preferences;
 mod components;
+mod view_build_log;
 
 pub async fn router(_state: AppState, _config: &Settings) -> Router<AppState, Body> {
     Router::new()
         .route_with_tsr("/new", get(create_project::get).post(create_project::post))
         .route_with_tsr("/:owner/:project", get(project_dashboard::get))
+        .route_with_tsr("/:owner/:project/builds/:build_id", get(view_build_log::get))
         .route_with_tsr("/:owner/:project/preferences", get(preferences::get))
         .route_with_tsr("/:owner/:project/delete", post(delete_project::post))
         .route_with_tsr("/:owner/:project/volume/delete", post(delete_volume::post))
---
+++ src/projects/view_build_log.rs
@@ -0,0 +1,134 @@
+use std::fmt;
+
+use axum::extract::{State, Path};
+use axum::response::Response;
+use hyper::{Body, StatusCode};
+use leptos::ssr::render_to_string;
+use leptos::{view, IntoView};
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+
+use crate::components::Base;
+use crate::projects::components::ProjectHeader;
+use crate::{auth::Auth, startup::AppState};
+
+#[derive(Serialize, Deserialize, Debug, sqlx::Type)]
+#[sqlx(type_name = "build_state", rename_all = "lowercase")] 
+pub enum BuildState {
+    PENDING,
+    BUILDING,
+    SUCCESSFUL,
+    FAILED
+}
+
+impl fmt::Display for BuildState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            BuildState::PENDING => write!(f, "Pending"),
+            BuildState::BUILDING => write!(f, "Building"),
+            BuildState::SUCCESSFUL => write!(f, "Successful"),
+            BuildState::FAILED => write!(f, "Failed"),
+        }
+    }
+}
+
+#[tracing::instrument(skip(auth, pool))]
+pub async fn get(
+    auth: Auth,
+    State(AppState { pool, domain, secure, .. }): State<AppState>,
+    Path((owner, project, build_id)): Path<(String, String, Uuid)>,
+) -> Response<Body> {
+    let _user = auth.current_user.unwrap();
+
+    // check if project exist
+    let _project_record = match sqlx::query!(
+        r#"SELECT projects.id, projects.name AS project, project_owners.name AS owner
+           FROM projects
+           JOIN project_owners ON projects.owner_id = project_owners.id
+           JOIN users_owners ON project_owners.id = users_owners.owner_id
+           AND projects.name = $1
+           AND project_owners.name = $2
+        "#,
+        project,
+        owner,
+    )
+    .fetch_optional(&pool)
+    .await
+    {
+        Ok(Some(record)) => record,
+        Ok(None) => {
+            let html = render_to_string(move || {
+                view! {
+                    <Base>
+                        <h1> Project does not exist </h1>
+                    </Base>
+                }
+            })
+            .into_owned();
+            return Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from(html))
+                .unwrap();
+        }
+        Err(err) => {
+            tracing::error!(?err, "Can't get projects: Failed to query database");
+            let html = render_to_string(move || {
+                view! {
+                    <h1> "Failed to query database " {err.to_string() } </h1>
+                }
+            })
+            .into_owned();
+            return Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from(html))
+                .unwrap();
+        }
+    };
+
+    let build = match sqlx::query!(
+        r#"SELECT id, project_id, status AS "status: BuildState", created_at, finished_at, log 
+        FROM builds WHERE id = $1
+        ORDER BY created_at DESC"#,
+        build_id
+    )
+    .fetch_one(&pool)
+    .await 
+    {
+        Ok(record) => record,
+        Err(err) => {
+            let html = render_to_string(move || {
+                view! {
+                    <h1> "Failed to query database " {err.to_string() } </h1>
+                }
+            })
+            .into_owned();
+            return Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from(html))
+                .unwrap();
+        }, 
+    };
+
+    let html = render_to_string(move || {
+        view! {
+            <Base>
+              <ProjectHeader owner={owner.clone()} project={project.clone()} domain={domain.clone()}></ProjectHeader>
+
+              <h2 class="text-xl">
+                "Build Log - ID: "{build.id.to_string()}
+              </h2>
+              <div class="w-full mt-4 px-1 mockup-code bg-neutral/40 backdrop-blur-sm">
+                {build.log.split("\n").map(|line| { view!{
+                    <pre><code>{line.to_string()}</code></pre>
+                }}).collect::<Vec<_>>()}
+              </div>
+            </Base>
+        }
+    })
+    .into_owned();
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from(html))
+        .unwrap()
+}
