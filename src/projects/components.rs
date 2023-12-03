5bae35ea85eff7f44f05e7736c8c944b0ddfc861
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-12-03 12:57:18 +0700

    fix: link to open website wrong link

--- src/projects/components.rs
+++ src/projects/components.rs
@@ -9,6 +9,7 @@ pub fn project_header(
     #[prop()]
     domain: String,
 ) -> impl IntoView {
+    let owner_dash = owner.replace(".", "-");
     view! {
         <div class="flex items-center justify-between mb-6">
             <div class="flex flex items-center">
@@ -35,7 +36,7 @@ pub fn project_header(
                         Terminal
                     </button>
                 </a>
-                <a href={format!("http://{}-{}.{}", &owner, &project, &domain)} target="_blank" rel="noreferrer">
+                <a href={format!("http://{}-{}.{}", &owner_dash, &project, &domain)} target="_blank" rel="noreferrer">
                     <button class="btn btn-sm btn-outline btn-primary gap-1">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"></path></svg>
                         Open
