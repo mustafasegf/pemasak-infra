38feee29efadca8eb1ccb587ad4411ba66941dbe
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-12-09 21:46:42 +0700

    feat: added table for showing envs

--- src/projects/preferences.rs
+++ src/projects/preferences.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
+
 use axum::extract::{Path, State};
 use axum::response::Response;
 use hyper::{Body, StatusCode};
 use leptos::ssr::render_to_string;
-use leptos::{view, IntoView};
+use leptos::{view, For, IntoView};
+use sqlx::types::Json;
 
 use crate::components::Base;
 use crate::projects::components::ProjectHeader;
@@ -20,8 +23,8 @@ pub async fn get(
     let volume_path = format!("/{owner}/{project}/volume/delete");
 
     // check if project exist
-    let _project = match sqlx::query!(
-        r#"SELECT projects.id, projects.name AS project, project_owners.name AS owner
+    let project_id = match sqlx::query!(
+        r#"SELECT projects.id
            FROM projects
            JOIN project_owners ON projects.owner_id = project_owners.id
            JOIN users_owners ON project_owners.id = users_owners.owner_id
@@ -34,7 +37,7 @@ pub async fn get(
     .fetch_optional(&pool)
     .await
     {
-        Ok(Some(record)) => record,
+        Ok(Some(record)) => record.id,
         Ok(None) => {
             let html = render_to_string(move || {
                 view! {
@@ -64,29 +67,77 @@ pub async fn get(
         }
     };
 
+    // type
+
+    #[derive(sqlx::FromRow)]
+    struct Envs {
+        envs: Json<HashMap<String, String>>,
+    }
+
+    let envs = match sqlx::query_as!(
+        Envs,
+        r#"SELECT envs as "envs: Json<HashMap<String, String>>" from projects where id = $1"#,
+        project_id,
+    )
+    .fetch_one(&pool)
+    .await
+    {
+        Ok(record) => record.envs.0,
+        Err(err) => {
+            tracing::error!(?err, "Can't get projects: Failed to query database");
+            let html = render_to_string(move || {
+                view! {
+                    <h1> "Failed to query database " {err.to_string() } </h1>
+                }
+            })
+            .into_owned();
+            return Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from(html))
+                .unwrap();
+        }
+    };
+
+    // TODO: add env modification
     let html = render_to_string(move || {
         view! {
             <Base is_logged_in={true}>
-              <ProjectHeader owner={owner} project={project} domain={domain}></ProjectHeader>
+                <ProjectHeader owner={owner} project={project} domain={domain}></ProjectHeader>
 
-              <h2 class="text-xl">
-                Project Controls
-              </h2>
-              <div class="flex w-full space-x-4 items-center mb-8">
-                <button
-                  hx-post={delete_path}
-                  hx-trigger="click"
-                  class="btn btn-error mt-4 w-full max-w-xs"
-                >Delete Project</button>
+                <h2 class="text-xl">
+                  Project Controls
+                </h2>
+                <div class="flex w-full space-x-4 items-center mb-8">
+                    <button
+                        hx-post={delete_path}
+                        hx-trigger="click"
+                        class="btn btn-error mt-4 w-full max-w-xs"
+                    >Delete Project</button>
 
-                <button
-                  hx-post={volume_path}
-                  hx-trigger="click"
-                  class="btn btn-error mt-4 w-full max-w-xs"
-                >Delete Database</button>
+                    <button
+                        hx-post={volume_path}
+                        hx-trigger="click"
+                        class="btn btn-error mt-4 w-full max-w-xs"
+                    >Delete Database</button>
               </div>
 
-              <div id="result"></div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Key</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                {envs.into_iter().map(|(key, value)| {
+                    view! {
+                        <tr>
+                            <td>{key}</td>
+                            <td>{value}</td>
+                        </tr>
+                    }
+                }).collect::<Vec<_>>()}
+            </table>
+            <div id="result"></div>
             </Base>
         }
     })
