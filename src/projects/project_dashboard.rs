0b76c94358db2d4ace3a936ec4b192ae568c088e
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-12-10 12:08:40 +0700

    refactor: project dashboard make it readable

--- src/projects/project_dashboard.rs
+++ src/projects/project_dashboard.rs
@@ -113,100 +113,109 @@ pub async fn get(
         }
     };
 
+    if builds.is_empty() {
+        let domain = domain.clone();
+        let owner = owner.clone();
+        let project = project.clone();
+        let protocol = match secure {
+            true => "https",
+            false => "http"
+        };
+
+        let html = render_to_string(move || {
+            view! {
+                <Base is_logged_in={true}>
+                    <ProjectHeader owner={owner.clone()} project={project.clone()} domain={domain.clone()}></ProjectHeader>
+
+                    <h2 class="text-xl">
+                        Builds
+                    </h2>
+                    <div class="flex flex-col gap-4 w-full mt-4">
+                        <div>
+                            <p class="mb-4">You have not pushed a build to your project, to push an existing project, execute the following command in your project</p>
+                            <div class="p-4 mb-4 bg-neutral/40 backdrop-blur-sm mockup-code" id="code">
+                                <pre>
+                                    <code>
+                                        "git remote add pws" {format!(" {protocol}://{domain}/{owner}/{project}")}
+                                    </code>
+                                </pre>
+                                <pre>
+                                    <code>
+                                        "git branch -M master" 
+                                    </code>
+                                </pre>
+                                <pre>
+                                    <code>
+                                        {"git push pws master"}
+                                    </code>
+                                </pre>
+                            </div>
+                            <button
+                                class="btn btn-outline btn-secondary mb-4"
+                                onclick="
+                                let lb = '\\n'
+                                if(navigator.userAgent.indexOf('Windows') != -1) {{
+                                lb = '\\r\\n'
+                                }}
+                
+                                let text = document.getElementById('code').innerText.replaceAll('\\n', lb)
+                                if ('clipboard' in window.navigator) {{
+                                    navigator.clipboard.writeText(text)
+                                }}"
+                            >
+                              Copy to clipboard
+                            </button>
+                        </div>
+                    </div>
+                </Base>
+            }
+        }).into_owned();
+
+        return Response::builder()
+            .status(StatusCode::OK)
+            .body(Body::from(html))
+            .unwrap();
+    }
+
     let html = render_to_string(move || {
         view! {
             <Base is_logged_in={true}>
-              <ProjectHeader owner={owner.clone()} project={project.clone()} domain={domain.clone()}></ProjectHeader>
+                <ProjectHeader owner={owner.clone()} project={project.clone()} domain={domain.clone()}></ProjectHeader>
 
-              <h2 class="text-xl">
-                Builds
-              </h2>
-              <div class="flex flex-col gap-4 w-full mt-4">
-                {
-                    match !builds.is_empty() {
-                        true => {
-                            builds.into_iter().enumerate().map(|(index, record)| { view!{
-                                <>
-                                    <a hx-boost="true" href={format!("/{}/{}/builds/{}", owner, project, record.id)} class="bg-neutral/40 backdrop-blur-sm text-info py-4 px-8 cursor-pointer w-full rounded-lg transition-all outline outline-transparent hover:outline-blue-500">
+                <h2 class="text-xl">
+                  Builds
+                </h2>
+                <div class="flex flex-col gap-4 w-full mt-4">
+                {builds.into_iter().enumerate().map(|(index, record)| { view!{
+                    <>
+                        <a hx-boost="true" href={format!("/{}/{}/builds/{}", owner, project, record.id)} class="bg-neutral/40 backdrop-blur-sm text-info py-4 px-8 cursor-pointer w-full rounded-lg transition-all outline outline-transparent hover:outline-blue-500">
+                        {
+                            let id = record.id.to_string();
+                            let status = record.status.to_string();
+                            let created_at = record.created_at;
+                            view!{
+                                <div class="text-sm">
+                                    <h2 class="font-bold text-white">
+                                        <span>{id}</span>
                                         {
-                                            let id = record.id.to_string();
-                                            let status = record.status.to_string();
-                                            let created_at = record.created_at;
+                                            let latest_build = match index == 0 {
+                                                true => " (LATEST BUILD)",
+                                                false => ""
+                                            };
+
                                             view!{
-                                                <div class="text-sm">
-                                                    <h2 class="font-bold text-white">
-                                                        <span>{id}</span>
-                                                        {
-                                                            let latest_build = match index == 0 {
-                                                                true => " (LATEST BUILD)",
-                                                                false => ""
-                                                            };
-                
-                                                            view!{
-                                                                <span class="text-info">{latest_build}</span>
-                                                            }
-                                                        }
-                                                    </h2>
-                                                    <p class="text-sm text-neutral-content">{"Status: "}{status}</p>
-                                                    <p class="text-sm text-neutral-content">{"Started at: "}{created_at.to_rfc2822()}</p>
-                                                </div>
+                                                <span class="text-info">{latest_build}</span>
                                             }
                                         }
-                                    </a>
-                                </>
-                            }}).collect::<Vec<_>>()
-                        },
-                        false => {
-                            let protocol = match secure {
-                                true => "https",
-                                false => "http"
-                            };
-                            
-                            vec!(
-                                view! {
-                                    <>
-                                        <div>
-                                            <p class="mb-4">You have not pushed a build to your project, to push an existing project, execute the following command in your project</p>
-                                            <div class="p-4 mb-4 bg-neutral/40 backdrop-blur-sm mockup-code" id="code">
-                                                <pre>
-                                                    <code>
-                                                        "git remote add pws" {format!(" {protocol}://{domain}/{owner}/{project}")}
-                                                    </code>
-                                                </pre>
-                                                <pre>
-                                                    <code>
-                                                        "git branch -M master" 
-                                                    </code>
-                                                </pre>
-                                                <pre>
-                                                    <code>
-                                                        {"git push pws master"}
-                                                    </code>
-                                                </pre>
-                                            </div>
-                                            <button
-                                                class="btn btn-outline btn-secondary mb-4"
-                                                onclick="
-                                                let lb = '\\n'
-                                                if(navigator.userAgent.indexOf('Windows') != -1) {{
-                                                lb = '\\r\\n'
-                                                }}
-                                
-                                                let text = document.getElementById('code').innerText.replaceAll('\\n', lb)
-                                                if ('clipboard' in window.navigator) {{
-                                                    navigator.clipboard.writeText(text)
-                                                }}
-                                            "
-                                            >
-                                            Copy to clipboard
-                                            </button>
-                                        </div>
-                                    </>
-                                }
-                            )
+                                    </h2>
+                                    <p class="text-sm text-neutral-content">{"Status: "}{status}</p>
+                                    <p class="text-sm text-neutral-content">{"Started at: "}{created_at.to_rfc2822()}</p>
+                                </div>
+                            }
                         }
-                    }
-                }
+                        </a>
+                    </>
+                }}).collect::<Vec<_>>()}
               </div>
             </Base>
         }
