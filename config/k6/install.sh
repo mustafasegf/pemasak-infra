98e604bab3af92ec3d5aec516e53b358c0ac92d4
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-12-11 13:12:53 +0700

    chore: simple k6 script to loop and clone git repo

---
+++ config/k6/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+ docker run --rm -u "$(id -u):$(id -g)" -v "${PWD}:/xk6" grafana/xk6 build \
+--with github.com/szkiba/xk6-yaml@latest \
+--with github.com/grafana/xk6-exec@latest \
+--with github.com/grafana/xk6-timers@latest \
+--with github.com/grafana/xk6-sql@latest \
+--with github.com/acuenca-facephi/xk6-read@latest
