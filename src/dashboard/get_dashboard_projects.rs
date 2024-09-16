8992ac1ded537952d72008a561c9209314d5327e
Adrian Ardizza <adrian.ardizza@gmail.com>
2024-09-16 12:13:13 +0700

    refactor: move dashboard code to separate module

--- src/dashboard/get_dashboard_projects.rs
+++ src/dashboard/get_dashboard_projects.rs
@@ -1,22 +1,11 @@
-use crate::auth::auth;
-use crate::configuration::Settings;
 use crate::{auth::Auth, startup::AppState};
-use axum::routing::get;
-use axum::{Router, middleware};
 use axum::extract::State;
 use axum::response::Response;
-use axum_extra::routing::RouterExt;
 use hyper::Body;
 use leptos::ssr::render_to_string;
 use leptos::{view, IntoView};
 use serde::Serialize;
 use uuid::Uuid;
-
-pub async fn router(_state: AppState, _config: &Settings) -> Router<AppState, Body> {
-    Router::new()
-        .route_with_tsr("/api/dashboard/project", get(get_dashboard_projects))
-        .route_layer(middleware::from_fn(auth))
-}
 
 #[derive(Serialize, Debug)]
 struct Project {
@@ -29,7 +18,7 @@ struct Project {
 struct DashboardProjectResponse {
     data: Vec<Project>
 }
-pub async fn get_dashboard_projects(auth: Auth, State(AppState { pool, .. }): State<AppState>) -> Response<Body> {
+pub async fn get(auth: Auth, State(AppState { pool, .. }): State<AppState>) -> Response<Body> {
     let user = auth.current_user.unwrap();
 
     let projects = match sqlx::query!(
